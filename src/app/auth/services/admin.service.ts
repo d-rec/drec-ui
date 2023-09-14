8aa45568fae42dbaca6a46114602c7f92ab7de50
NamrataSweya <102014274+NamrataSweya@users.noreply.github.com>
2023-09-14 17:41:55 +0530

    add pagination in all organization list and all user list in admin

--- src/app/auth/services/admin.service.ts
+++ src/app/auth/services/admin.service.ts
@@ -10,36 +10,47 @@ export class AdminService {
   url: String = environment.API_URL;
   constructor(private httpClient: HttpClient) { }
 
-  public GetAllOrganization(): Observable<any> {
-    return this.httpClient.get<any>(this.url + 'admin/organizations' );
+  public GetAllOrganization(pagenumber?:number,limit?:number): Observable<any> {
+    let searchUrl = `${this.url}admin/organizations`;
+    if (pagenumber != undefined && limit != undefined ) {
+      if (!(typeof pagenumber === undefined || pagenumber === null)) {
+        searchUrl += `?pageNumber=${pagenumber}&limit=${limit}` ;
+      }
+    }
+    return this.httpClient.get<any>(searchUrl);
   }
-  public GetOrganizationById(orgId:number): Observable<any> {
-    return this.httpClient.get<any>(this.url + 'admin/organizations/'+orgId );
+  public GetOrganizationById(orgId: number): Observable<any> {
+    return this.httpClient.get<any>(this.url + 'admin/organizations/' + orgId);
   }
-  public GetAllUsers(searchData?:any): Observable<any> {
+  public GetAllUsers(pagenumber?:number,limit?:number,searchData?: any): Observable<any> {
     let searchUrl = `${this.url}admin/users`;
+    if (pagenumber != undefined && limit != undefined ) {
+      if (!(typeof pagenumber === undefined || pagenumber === null)) {
+        searchUrl += `?pageNumber=${pagenumber}&limit=${limit}` ;
+      }
+    }
     if (searchData != undefined) {
       if (!(typeof searchData.organizationName === undefined || searchData.organizationName === "" || searchData.organizationName === null)) {
-        searchUrl += `?organizationName=${searchData.organizationName}`;
+        searchUrl += `&organizationName=${searchData.organizationName}`;
       }
     }
     return this.httpClient.get(searchUrl);
   }
-  public GetAllOrgnaizationUsers(organizationsId:number): Observable<any> {
-    return this.httpClient.get<any>(this.url + 'admin/organizations/user/'+organizationsId );
+  public GetAllOrgnaizationUsers(organizationsId: number): Observable<any> {
+    return this.httpClient.get<any>(this.url + 'admin/organizations/user/' + organizationsId);
   }
-  public updateUser(userId:number,data:any):Observable<any>{
-    return this.httpClient.put<any>(this.url+'admin/users/'+userId,data)
+  public updateUser(userId: number, data: any): Observable<any> {
+    return this.httpClient.put<any>(this.url + 'admin/users/' + userId, data)
   }
-  public removeUser(userId:number):Observable<any>{
-    return this.httpClient.delete<any>(this.url+'admin/user/'+userId)
+  public removeUser(userId: number): Observable<any> {
+    return this.httpClient.delete<any>(this.url + 'admin/user/' + userId)
   }
-  GetDeviceAutocomplete(searchInput: StaticRange,orgId:number): Observable<any> {
-    let searchUrl = `${this.url}admin/devices/autocomplete?externalId=` + searchInput+`&organizationId=`+orgId;
+  GetDeviceAutocomplete(searchInput: StaticRange, orgId: number): Observable<any> {
+    let searchUrl = `${this.url}admin/devices/autocomplete?externalId=` + searchInput + `&organizationId=` + orgId;
     return this.httpClient.get(searchUrl);
 
   }
-  public AddIrecDevice(deviceId:any): Observable<any> {
-    return this.httpClient.post<any>(this.url + 'admin/add/device-into-Irec/'+deviceId ,{});
+  public AddIrecDevice(deviceId: any): Observable<any> {
+    return this.httpClient.post<any>(this.url + 'admin/add/device-into-Irec/' + deviceId, {});
   }
 }
