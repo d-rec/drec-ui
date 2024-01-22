a5db848b1b09f2d967ddbfa6648fd7e6a04b4445
NamrataSweya <102014274+NamrataSweya@users.noreply.github.com>
2024-01-22 12:36:28 +0530

    Fix invitation  issue in stage

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Input, Component, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { AuthbaseService } from '../../auth/authbase.service';
-import { UserService } from '../../auth/services';
+import { UserService,InvitationService } from '../../auth/services';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 @Component({
@@ -17,7 +17,8 @@ export class LoginComponent {
   // loginForm: FormGroup;
   constructor(private authService: AuthbaseService,
     private router: Router, private toastrService: ToastrService,
-    private userService: UserService) {
+    private userService: UserService,
+    private inviteservice: InvitationService) {
 
   }
   // submit() {
@@ -49,7 +50,7 @@ export class LoginComponent {
               console.log(data1)
               sessionStorage.setItem('status', data1.status);
               if (data1.status != 'Pending' && data1.organization != null) {
-                if (jwtObj.role === 'Buyer') {
+                if (data1.organization.organizationType === 'Buyer') {
                   this.router.navigate(['/myreservation']);
                 } else if (jwtObj.role === 'Admin') {
                   this.router.navigate(['/admin/All_devices']);
@@ -57,9 +58,35 @@ export class LoginComponent {
                   console.log("50developer")
                   this.router.navigate(['/device/AllList']);
                 }
-                this.toastrService.success('login user ' + jwtObj.email + '!', 'login Success');
-              }else{
-                this.router.navigate(['/organization/user/invitation']);
+                this.toastrService.success('Login user ' + jwtObj.email + '!', 'Login Success');
+              } else {
+
+                this.inviteservice.getinvitaion().subscribe({
+                  next: data => {
+                    console.log(data);
+                    const invitationId = data[0].id
+                    //@ts-ignore
+                    let loginuser = JSON.parse(sessionStorage.getItem('loginuser'));
+
+                    // Update the role property of the loginuser object with the new value
+                    loginuser.role = data[0].role;
+                    console.log(loginuser);
+
+                    // Save the updated loginuser object back to sessionStorage
+                    sessionStorage.setItem('loginuser', JSON.stringify(loginuser));
+                    this.inviteservice.acceptinvitaion(invitationId, {
+                      email: jwtObj.email,
+                      status: "Accepted"
+                    }).subscribe({
+                      next: data => {
+                        console.log(data);
+                        this.toastrService.success('Accept Sucessful!', 'Invitation ');
+                        this.onSubmit();
+                      }
+                    })
+                  }
+                })
+                //this.router.navigate(['/organization/user/invitation']);
 
               }
 
@@ -69,15 +96,59 @@ export class LoginComponent {
           })
         } else {
           console.log("check your credentials !!")
-          this.toastrService.info('Message Failure!', 'check your credentials !!');
+          this.toastrService.info('Message Failure!', 'Check Your Credentials !!');
           this.router.navigate(['/login']);
         }
       },
       (error) => {                              //Error callback
         console.error('error caught in component', error)
-        this.toastrService.error('check your credentials!', 'login Fail!!');
+        this.toastrService.error('Check Your Credential!', 'Login Fail!!');
       }
     )
+    // this.authService.login('auth/login', this.loginForm.value).subscribe(
+    //   (data) => {
+
+    //     if (data["accessToken"] != null) {
+    //       sessionStorage.setItem('access-token', data["accessToken"]);
+    //       let jwtObj = JSON.parse(this.b64DecodeUnicode(this.padBase64(data["accessToken"].split('.')[1])));
+    //       console.log(jwtObj);
+    //       //sessionStorage.setItem('loginuser', jwtObj);
+    //       sessionStorage.setItem('loginuser', JSON.stringify(jwtObj));
+    //       //var obj = JSON.parse(sessionStorage.loginuser);
+    //       this.userService.userProfile().subscribe({
+    //         next: data1 => {
+    //           console.log(data1)
+    //           sessionStorage.setItem('status', data1.status);
+    //           if (data1.status != 'Pending' && data1.organization != null) {
+    //             if (jwtObj.role === 'Buyer') {
+    //               this.router.navigate(['/myreservation']);
+    //             } else if (jwtObj.role === 'Admin') {
+    //               this.router.navigate(['/admin/All_devices']);
+    //             } else {
+    //               console.log("50developer")
+    //               this.router.navigate(['/device/AllList']);
+    //             }
+    //             this.toastrService.success('login user ' + jwtObj.email + '!', 'login Success');
+    //           }else{
+    //             this.router.navigate(['/organization/user/invitation']);
+
+    //           }
+
+    //         }, error: err => {
+    //           this.toastrService.error('Error!', err.error.message);
+    //         }
+    //       })
+    //     } else {
+    //       console.log("check your credentials !!")
+    //       this.toastrService.info('Message Failure!', 'check your credentials !!');
+    //       this.router.navigate(['/login']);
+    //     }
+    //   },
+    //   (error) => {                              //Error callback
+    //     console.error('error caught in component', error)
+    //     this.toastrService.error('check your credentials!', 'login Fail!!');
+    //   }
+    // )
   }
 
   @Output() submitEM = new EventEmitter();
