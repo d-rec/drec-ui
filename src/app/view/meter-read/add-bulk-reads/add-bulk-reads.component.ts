cc155f7d9631331718e80453a6517c8d41ecada8
charlotte <divinemaina@gmail.com>
2024-10-09 17:06:40 +0200

    Add a UI for bulk meter read upload using CSV

---
+++ src/app/view/meter-read/add-bulk-reads/add-bulk-reads.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FileuploadService } from '../../../auth/services/fileupload.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { AdminService, OrganizationService } from '../../../auth/services';
+import { OrganizationInformation } from '../../../models';
+
+@Component({
+  selector: 'app-add-bulk-reads',
+  templateUrl: './add-bulk-reads.component.html',
+  styleUrls: ['./add-bulk-reads.component.scss'],
+})
+export class AddBulkReadsComponent implements OnInit {
+  currentFile?: File | null;
+  progress = 0;
+  message = '';
+  pageSize: number = 10;
+  fileName = 'Please click here to select file';
+  fileInfos?: Observable<any>;
+  showmeterreadinfo: boolean = false;
+  MeterreadstatusList: any = [];
+  loading: boolean = true;
+  objectKeys = Object.keys;
+  displayedColumns = [
+    'serialno',
+    'createdAt',
+    'jobId',
+    'fileId',
+    'status',
+    'actions',
+  ];
+  displayedColumns1 = [
+    'serialno',
+    'externalId',
+    'errorsList',
+    'Status',
+    'Action',
+  ];
+  constructor(
+    private uploadService: FileuploadService,
+    // private router: Router,
+    // private toastrService: ToastrService,
+    private adminService: AdminService,
+    private orgService: OrganizationService,
+  ) {
+    this.loginuser = JSON.parse(sessionStorage.getItem('loginuser')!);
+  }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: MatTableDataSource<any>;
+  dataSource1: MatTableDataSource<any>;
+  data: any;
+  orglist: any;
+  filteredOrgList: OrganizationInformation[] = [];
+  orgname: string;
+  orgId: number;
+  loginuser: any;
+
+  ngOnInit(): void {
+    if (this.loginuser.role === 'Admin') {
+      this.adminService.GetAllOrganization().subscribe((data) => {
+        this.orglist = data.organizations.filter(
+          (org: OrganizationInformation) => org.organizationType !== 'Buyer',
+        );
+        this.filteredOrgList = this.orglist;
+      });
+    } else if (this.loginuser.role === 'ApiUser') {
+      this.orgService.GetApiUserAllOrganization().subscribe((data) => {
+        this.orglist = data.organizations.filter(
+          (org) => org.organizationType != 'Buyer',
+        );
+        this.filteredOrgList = this.orglist;
+      });
+    }
+    this.JobDisplayList();
+  }
+
+  filterOrgList() {
+    this.filteredOrgList = this.orglist.filter(
+      (org: OrganizationInformation) => {
+        return org.name.toLowerCase().includes(this.orgname.toLowerCase());
+      },
+    );
+  }
+  selectOrg(event: any) {
+    const selectedCountry = this.orglist.find(
+      (option: any) => option.name === event.option.value,
+    );
+    if (selectedCountry) {
+      this.orgId = selectedCountry.id;
+    }
+  }
+  reset() {
+    this.currentFile = null;
+    this.fileName = 'Please click here to select file';
+  }
+  selectFile(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      const file: File = event.target.files[0];
+      this.currentFile = file;
+      this.fileName = this.currentFile.name;
+      if (!this.fileName.endsWith('.csv')) {
+        this.fileName = 'Invalid file';
+        this.currentFile = null;
+      }
+    } else {
+      this.fileName = 'Please click here to select file';
+    }
+    event.target.value = '';
+  }
+
+  openFileExplorer() {
+    document.getElementById('fileInput')?.click();
+  }
+
+  upload(): void {
+    this.progress = 0;
+    this.message = '';
+  }
+  JobDisplayList() {
+    this.showmeterreadinfo = false;
+    this.loading = true;
+    this.uploadService.getCsvJobList().subscribe((data) => {
+      // display list in the console
+      this.loading = false;
+      this.data = data;
+      this.dataSource = new MatTableDataSource(this.data.csvJobs);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+}
--- src/app/view/meter-read/meter-read-routing.module.ts
+++ src/app/view/meter-read/meter-read-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddreadComponent } from './addread/addread.component';
 import { AllMetereadsComponent } from '../meter-read/all-metereads/all-metereads.component';
+import { AddBulkReadsComponent } from './add-bulk-reads/add-bulk-reads.component';
 const routes: Routes = [
   { path: '', redirectTo: 'Allreads', pathMatch: 'full' },
   { path: 'All', component: AllMetereadsComponent },
   { path: 'add', component: AddreadComponent },
-  // { path: 'bulk_upload', component: AddBulkDeviceComponent },
+  { path: 'bulk_upload', component: AddBulkReadsComponent },
   // { path: 'edit/:id', component: EditDeviceComponent },
 ];
 
