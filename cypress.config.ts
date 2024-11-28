ff53d9e3556545ed22d1fe4edac59557639c9838
rithaven <ritha@drec.enery>
2024-11-28 10:33:07 +0200

    fix:GitHub Actions workflow  testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200', // Frontend URL
+    baseUrl: 'http://localhost:4200',
     env: {
-      REACT_APP_BACKEND_URL: 'http://localhost:3040', // Backend URL
+      REACT_APP_BACKEND_URL: 'http://localhost:3040',
     },
   },
 });
