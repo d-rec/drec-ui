d646fe59a3667670d03acf3e886e88cbee83dd72
rithaven <ritha@drec.enery>
2024-12-01 12:06:12 +0200

    fix:GitHub Actions workflow  testing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('clearDatabase', () => {
   cy.request({
     method: 'DELETE',
-    url: "http://localhost:3040/api/testing/clear-db",
+    url: "http://localhost:3040/api/testing/clear-db", 
     failOnStatusCode: false, 
   }).then((response) => {
     if (response.status === 200) {
