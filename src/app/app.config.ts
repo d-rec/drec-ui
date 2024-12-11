8f6c2e1d859848c3da1a7eb1120a597291258fc1
charlotte <divinemaina@gmail.com>
2024-12-11 16:51:34 +0200

    fix:fix linters and prettier

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,19 +2,23 @@ import { APP_INITIALIZER, ErrorHandler } from '@angular/core';
 import { Router } from '@angular/router';
 import * as Sentry from '@sentry/angular';
 import { environment } from 'src/environments/environment';
-if ( environment.production) {
-Sentry.init({
-  dsn: environment.SENTRY_DNS,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ['localhost', /^https?:\/\/localhost:\d+/],
-  environment: environment.production ? 'production' : environment.staging ? 'staging' : 'development',
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+if (environment.production) {
+  Sentry.init({
+    dsn: environment.SENTRY_DNS,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ['localhost', /^https?:\/\/localhost:\d+/],
+    environment: environment.production
+      ? 'production'
+      : environment.staging
+        ? 'staging'
+        : 'development',
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
 }
 export const appConfig: any = {
   providers: [
