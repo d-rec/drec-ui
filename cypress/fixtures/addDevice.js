50325cd863a94684b9242323403477dfbc7c70ee
rithaven <ritha@drec.enery>
2024-12-18 10:46:17 +0200

    ft:add automated tests for meter read feature

--- cypress/fixtures/addDevice.js
+++ cypress/fixtures/addDevice.js
@@ -17,7 +17,7 @@
       {
         action: "type",
         selector: '[data-testid="external-id"]',
-        value:"testdevice00"
+        value:"testdevice20"
       },
       {
         action: "select",
