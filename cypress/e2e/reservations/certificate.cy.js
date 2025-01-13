f83ba2f6e8d45e97f3bc3ac34bb3e6e6ba6b1978
rithaven <ritha@drec.enery>
2025-01-13 18:23:11 +0200

    chore: update workflow branches

--- cypress/e2e/reservations/certificate.cy.js
+++ cypress/e2e/reservations/certificate.cy.js
@@ -2,7 +2,6 @@ describe('Add reservation', () => {
     beforeEach(()=>{
       cy.wait(1000);
     })
-    
     it('should add reservation', () => {
       cy.buyerlogin().wait(20000);
       cy.certficate();
