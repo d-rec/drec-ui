5d807e1259e64366366d5cced658ea43a63a4886
rithaven <ritha@drec.enery>
2025-01-13 10:10:59 +0200

    fix: resolve conflicts

--- cypress/e2e/device/add-devices.cy.js
+++ cypress/e2e/device/add-devices.cy.js
@@ -2,6 +2,7 @@ describe('Add divice Test', () => {
     beforeEach(()=>{
       cy.wait(1000);
     })
+    
     it('should add new divice', () => {
       cy.devlogin().wait(6000);
       cy.addDevice();
