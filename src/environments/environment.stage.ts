d29acc976b8156a1727a28a4b951af28eb4aef80
Yannick Musafiri <yannick@musafiri.co>
2025-01-14 10:24:20 +0200

    chore: release logo changes and code cleanups (#188)
    * feat: removed form-field folder
    
    * remove interceptor folder and its content
    
    * Feat: Removed getNamesFromCode file
    
    * Feat:Added favicons
    
    * fix README typo
    
    * Feat: Updated password hint
    
    * Added the field message
    
    * Feat: Updated the typo
    
    * add punctuation on CHANGELOG.md texts
    
    * Feat: Added a default time frame
    
    * Fixed prettier
    
    * remove unwanted angular image
    
    * add clear unauthorized message for certificates and user-invitation
    
    * Added value when initializing
    
    * Removed empty space
    
    * Created startDate variable
    
    * Added prettier
    
    * Feat: Updating the d-rec logo
    
    * Updating the logo
    
    * feat: increase version number
    
    * chore: add contribution guidelines (#196)
    
    * feat: allow creating devices with negative coordinates (#200)
    
    * Ft/integrate sentry (#206)
    
    * feat:intergrate sentry in the project
    
    * feat:intergrate sentry in the project
    
    * fix:fix linters
    
    * fix:fix environment
    
    * fix:remove the sessions
    
    * implement requested changes
    
    * fix:fix linters and prettier
    
    * implement requested changes
    
    * fix:remove the duplicate enevironment check
    
    * feat: set up Cypress for E2E testing (#201)
    
    * ft: set up Cypress for E2E testing
    
    * ft: set up Cypress for E2E testing
    
    * feat: change navigation icons to make navigation easy
    
    * feat: group reservation under the same menu
    
    * fix: build issue on dev
    
    * chore: increase version number
    
    ---------
    
    Co-authored-by: Kingcedru <byishimocedrick@gmail.com>
    Co-authored-by: root <root@DESKTOP-KSAAPGB>
    Co-authored-by: rithaven <veneruth4@gmail.com>
    Co-authored-by: charlotte <divinemaina@gmail.com>
    Co-authored-by: Kankindi Ritha Veneranda <52707943+rithaven@users.noreply.github.com>

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -4,8 +4,12 @@
 
 export const environment = {
   production: false,
+  staging: true,
   API_URL: 'https://stage-api.drecs.org/api/',
   Explorer_URL: 'https://volta-explorer.energyweb.org',
+  SENTRY_DNS:
+    'https://5447c8011f4b40f4dcb8742dfbaa1c0e@o4508380579430400.ingest.de.sentry.io/4508380582576208',
+  SENTRY_ENV: 'staging',
 };
 
 /*
