7d639e94e2783ad0cec1f3c2ec8903100e565c6a
rithaven <ritha@drec.enery>
2025-01-22 17:10:12 +0200

    Resolved merge conflicts

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -101,6 +101,26 @@ Cypress.Commands.add('permissions', function () {
   });
 });
 
+Cypress.Commands.add('dev_login', function () {
+  
+  cy.fixture('dev_login.js').then((data) => {
+    cy.visit(`${UI_BASE_URL}/login`).wait(1000); 
+    data.forEach((step) => {
+  
+    
+      if (step.action === "type") {
+        
+          return cy.get(step.selector).type(step.value);
+        }
+        
+    if (step.action === "click") {
+      return cy.get(step.selector).click().wait(1000);
+    }
+  });
+  });
+});
+
+
 Cypress.Commands.add('add_device', function () {
   
   cy.fixture('add_device.js').then((data) => { 
@@ -131,21 +151,55 @@ Cypress.Commands.add('add_device', function () {
   });
 });
 
-Cypress.Commands.add('dev_login', function () {
+Cypress.Commands.add('add_meter_read', function () {
   
-  cy.fixture('dev_login.js').then((data) => {
-    cy.visit(`${UI_BASE_URL}/login`).wait(1000); 
+  cy.fixture('add_meter_read.js').then((data) => { 
     data.forEach((step) => {
-  
-    
-      if (step.action === "type") {
-        
-          return cy.get(step.selector).type(step.value);
-        }
-        
     if (step.action === "click") {
       return cy.get(step.selector).click().wait(1000);
+    }
+    if (step.action === "selected") {
+       return  cy.get(step.selector).click({ force: true }).wait(1000)
+           .get(step.option)
+           .should('have.length.greaterThan', 0)
+           .first() 
+           .should('be.visible') 
+           .click({ force: true }) 
+           .wait(1000); 
+}
+
+    
+    if (step.action === "select-timezone") {
+      return cy.get(step.selector).click({ force: true })
+        .get(step.option) 
+        .should('have.length.greaterThan', 0) 
+        .eq(0)
+        .click('center', { force: true }); 
+    }
+    if (step.action === "select") {
+      return cy.get(step.selector).click({ force: true })
+        .get(step.option) 
+        .should('have.length.greaterThan', 0) 
+        .eq(0)
+        .click('center', { force: true }); 
+    }
+    
+    if (step.action === "type") {
+      return cy.get(step.selector).wait(1000)  
+      .click({ force: true }) 
+      .type(step.value, { force: true });
+    }
+    if (step.action === "start-date") {
+      return cy.get(step.selector).eq(0).click('center', { force: true })
+              .get(step.option).eq(0).click()
+              .get(".mat-stroked-button").click();
+    }
+    if (step.action === "end-date") {
+      return cy.get(step.selector).eq(1).click('center', { force: true })
+              .get(step.option).eq(5).click()
+              .get(".mat-stroked-button").click();
     }
   });
   });
 });
+
