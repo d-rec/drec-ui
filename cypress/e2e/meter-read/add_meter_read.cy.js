d4fc7916fa39787009771977a5b638df538a4e6a
rithaven <ritha@drec.enery>
2025-01-27 11:48:56 +0200

    fix: remove duplicate files cypress/e2e

--- cypress/e2e/meter-read/add_meter_read.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Add meter Read', () => {
-  beforeEach(() => {
-    cy.wait(1000);
-  });
-  it('should add meter Read', () => {
-    cy.dev_login().wait(20000);
-    cy.add_meter_read();
-  });
-});
