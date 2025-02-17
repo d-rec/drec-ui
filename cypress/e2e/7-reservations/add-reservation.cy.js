bfde9190d1b3d7425e07850168d99aa2d9c08057
rithaven <veneruth4@gmail.com>
2025-02-17 11:04:36 +0200

    fix: remove unnecessary folder

--- cypress/e2e/7-reservations/add-reservation.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Add reservation', () => {
-  beforeEach(() => {
-    cy.wait(1000);
-  });
-  it('should add reservation', () => {
-    cy.buyerUserLogin().wait(20000);
-    cy.addReservation();
-  });
-});
