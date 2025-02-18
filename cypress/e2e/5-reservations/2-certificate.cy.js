39a1d412f2cb89e1161a2606eb16874d2fb06563
Kankindi Ritha Veneranda <52707943+rithaven@users.noreply.github.com>
2025-02-18 18:24:39 +0200

    feat: add autometed test for account setting (#239)
    * feat: add autometed test for account setting
    
    * fx: updated fixtures file
    
    * fx: installed missing dependecies
    
    * fx: installed missing cypress
    
    * fix: update fixtures/account_settings.js
    
    * fix: update fixtures/add_meter_read.js
    
    * fix: improve test descriptions and clarity
    
    * fix: improve test descriptions in cypress/e2e/d-permissions
    
    * fix: remove duplicated file
    
    * refactor: rename files to kebab-case and update data-testid attributes
    
    * fix: remove unnecessary
    
    * fix: updated buyer-user-permissions-setup
    
    * chore: add aria-label for add User Role Permissions tab
    
    * fix: rename test ID to follow kebab-case format
    
    * fix: remove duplicate files
    
    * fix:add missing test ID for account settings header
    
    * fix: remove duplicate folder numbering to avoid conflicts

--- cypress/e2e/5-reservations/2-certificate.cy.js
+++ cypress/e2e/5-reservations/2-certificate.cy.js
@@ -1,8 +1,8 @@
-describe('Add reservation', () => {
+describe('Certificate Generation', () => {
   beforeEach(() => {
     cy.wait(1000);
   });
-  it('should add reservation', () => {
+  it('should generate a certificate successfully', () => {
     cy.buyerUserLogin().wait(20000);
     cy.certificate();
   });
--- cypress/fixtures/developer-user-permissions-setup.js
+++ cypress/fixtures/developer-user-permissions-setup.js
@@ -1,45 +1,84 @@
 [
   {
     action: 'click',
-    selector: '[test-id="permission"]',
-  },
-
-  {
-    action: 'click',
-    selector: '[test-id="user-role-permission"]',
+    selector: '[test-id="permission"]'
   },
   {
     action: 'click',
-    selector: '.add-user-role-permissions',
+    selector: '[test-id="user-role-permission"]'
+  },
+  {
+    action: 'click',
+    selector: '[aria-label="add-user-role-permissions-test-id"]'
   },
   {
     action: 'select',
     selector: '[test-id="add-user-role"]',
-    option: '[test-id="user-role-option"]',
+    option: '[test-id="user-role-option"]'
   },
   {
     action: 'check',
     selector: '[test-id="select-module"]',
-    index: 3,
+    index: 1
   },
   {
     action: 'check-multiple',
     selector: '[test-id="select-permission"]',
-    contains: 'DEVICE_MANAGEMENT_CRUDL',
+    contains: 'ORGANIZATION_MANAGEMENT_CRUDL'
   },
   {
     action: 'check',
     selector: '[test-id="select-module"]',
-    index: 6,
+    index: 3
   },
   {
     action: 'check-multiple',
     selector: '[test-id="select-permission"]',
-    contains: 'READS_MANAGEMENT_CRUDL',
+    contains: 'DEVICE_MANAGEMENT_CRUDL'
+  },
+  {
+    action: 'check',
+    selector: '[test-id="select-module"]',
+    index: 5
+  },
+  {
+    action: 'check-multiple',
+    selector: '[test-id="select-permission"]',
+    contains: 'DEVICE_BULK_MANAGEMENT_CRUDL'
+  },
+  {
+    action: 'check',
+    selector: '[test-id="select-module"]',
+    index: 6
+  },
+  {
+    action: 'check-multiple',
+    selector: '[test-id="select-permission"]',
+    contains: 'READS_MANAGEMENT_CRUDL'
   },
 
   {
-    action: 'submit',
-    selector: '[test-id="submit-permission"]',
+    action: 'check',
+    selector: '[test-id="select-module"]',
+    index: 7
   },
+  {
+    action: 'check-multiple',
+    selector: '[test-id="select-permission"]',
+    contains: 'CERTIFICATE_LOG_MANAGEMENT_CRUDL'
+  },
+  {
+    action: 'check',
+    selector: '[test-id="select-module"]',
+    index: 10
+  },
+  {
+    action: 'check-multiple',
+    selector: '[test-id="select-permission"]',
+    contains: 'PASSWORD_MANAGEMENT_CRUDL'
+  },
+  {
+    action: 'submit',
+    selector: '[test-id="submit-permission"]'
+  }
 ]
