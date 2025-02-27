c09b8455191cff9047d18dc9d900271396a86750
Kankindi Ritha Veneranda <52707943+rithaven@users.noreply.github.com>
2025-02-27 12:37:20 +0200

    Ft/sequential cypress tests browser (#257)
    * feat: add automated test for bulk upload
    
    * fix: update bulk_upload.js
    
    * fix: update bulk-upload.js with missing test ID
    
    * fix: update fuxtures/bulk-upload
    
    * fix: add bulk file into fixtures for cypress use
    
    * fix: add waiting time to bulkUpload to upload the file
    
    * fix: updated commands.js
    
    * fix: import cypress-file-upload in commands.js to enable file upload functionality
    
    * refactor: update test ID to use kebab case
    
    * fix: modify test id
    
    * fix: change the test id naming
    
    * refactor: rename files and update test-id to kebab-case
    
    * fix: correct typo, rename files and test-id to kebab-case
    
    * feat: add automated test for late ongoing meter reads
    
    * fix: modified late-ongoing-meter-read.js
    
    * fix: modified fixtures file
    
    * fix: modified developer-user-login.js
    
    * fix: modified fixtures/developer-user-permissions-setup.js
    
    * fix:failing test due to missing [external-id-test-id] element~
    
    * fix:failing test due to missing test-id
    
    * fix:failing test due to missing test-id
    
    * fix:failing test due to missing test-id=permission
    
    * fix: increase timeout to resolve CI test failure
    
    * fix: increase waiting time
    
    * fix: extend wait time to improve reliability
    
    * fix: increase timeout to resolve CI test failure
    
    * fix:increase Cypress default timeout to fix test failure
    
    * fix: add miss test id
    
    * fix: modify cypress.config.ts
    
    * fix: add missing test id
    
    * fix: remove typo error on added test id
    
    * fix: remove duplicated files
    
    * fix: remove unnecessary folder
    
    * fix: added missing e2e file
    
    * fix: remove unnecessary comments
    
    * feat: add automated test for certificate filtering
    
    * fix: change file name
    
    * fix: add missing test id
    
    * fix: fix the typo error
    
    * fix: apply requested changes from PR review
    
    * fix: resolve syntax error in certificate-filter.js
    
    * fix: add missing test id
    
    * fix: add missing test id for choosing end date
    
    * fix: resolve syntax error in history meter read test
    
    * feat: add automated test for aggregate meter read
    
    * fix: add missing test ID
    
    * fix: add missing test ID for read-value
    
    * fix: add missing test ID for history meter reads
    
    * fix: fix the test id naming
    
    * fix: Resolve test failure
    
    * feat: enable sequential execution of Cypress test files in browser mode
    
    * hore: rename test file to 01-admin-login.cy.js

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    env: {
-      REACT_APP_BACKEND_URL: 'http://localhost:3040',
-      ADMIN_EMAIL: 'drec@energy.org',
-      ADMIN_PASSWORD: '1234Rc',
-      UI_BASE_URL: 'http://localhost:4200',
-    },
+    experimentalRunAllSpecs: true,
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+  },
+  env: {
+    REACT_APP_BACKEND_URL: 'http://localhost:3040',
+    ADMIN_EMAIL: 'drec@energy.org',
+    ADMIN_PASSWORD: '1234Rc',
+    UI_BASE_URL: 'http://localhost:4200',
   },
 });
