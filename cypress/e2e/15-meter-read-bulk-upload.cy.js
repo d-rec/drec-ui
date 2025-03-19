4f8111c1ca6d9472d4c77bdc2b563db8ee740280
Kankindi Ritha Veneranda <52707943+rithaven@users.noreply.github.com>
2025-03-19 10:09:28 +0200

    Ft/meter read bulk upload test (#275)
    * feat: add automated test for meter read bulk upload
    
    * feat: add automated test for meter read bulk upload
    
    * chore: remove commented-out code in account settings test
    
    * refactor: remove unused plugins
    
    * chore: restore package-lock.json
    
    ---------
    
    Co-authored-by: uwumukiza123 <uwumukiza123>
    Co-authored-by: Yannick Musafiri <yannick@musafiri.co>

--- cypress/e2e/15-meter-read-bulk-upload.cy.js
+++ cypress/e2e/15-meter-read-bulk-upload.cy.js
@@ -4,7 +4,7 @@ describe('Bulk Upload Test', () => {
       });
     it('should upload a file', () => {
       cy.developerUserLogin().wait(60000);
-      cy.deviceBulkUpload('device-bulk-upload-template'); 
+      cy.meterReadBulkUpload('meter-bulk-upload-template'); 
     });
   });
   
