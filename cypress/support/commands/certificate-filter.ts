b1478d5519aad219dd941e45b52d7c4d1adc81a4
uwumukiza123 <uwumukiza123>
2025-03-24 19:33:29 +0200

    refactor: automated test cases

--- cypress/support/commands/certificate-filter.ts
+++ cypress/support/commands/certificate-filter.ts
@@ -3,26 +3,39 @@ import 'cypress-file-upload';
 Cypress.Commands.add('certificateFilter', function () {
   cy.fixture('certificate-filter.json').then((data) => {
     data.forEach((step) => {
-      if (step.action === 'click') {
-        cy.get(step.selector).click().wait(1000);
-      }
-      if (step.action === 'select') {
-        cy.get(step.selector).click({ force: true });
-        cy.get('body').then(($body) => {
-          if ($body.find(step.option).length > 0) {
-            cy.get(step.option)
-              .should('have.length.greaterThan', 0)
-              .eq(0)
-              .click('center', { force: true });
-            cy.get('[test-id="filter-button"]').click({ force: true });
-          } else {
-            cy.get('[test-id="dropdown-no-selection"]').click({ force: true });
-            cy.get('[test-id="filter-button"]').click({ force: true });
-            cy.get('[test-id="no-certificate"]', { timeout: 5000 })
-              .should('be.visible')
-              .should('contain', 'No Certificate');
-          }
-        });
+      switch (step.action) {
+        case 'click':
+          cy.get(step.selector).click();
+          break;
+        case 'select':
+          cy.get(step.selector).click({ force: true });
+          cy.get('body').then(($body) => {
+            switch ($body.find(step.option).length > 0) {
+              case true:
+                cy.get(step.option)
+                  .should('have.length.greaterThan', 0)
+                  .eq(0)
+                  .click('center', { force: true });
+                cy.get('[test-id="filter-button"]').click({ force: true });
+                break;
+              case false:
+                cy.get('[test-id="dropdown-no-selection"]').click({
+                  force: true,
+                });
+                cy.get('[test-id="filter-button"]').click({ force: true });
+
+                cy.get('[test-id="no-certificate"]', { timeout: 5000 })
+                  .should('exist')
+                  .then(($el) => {
+                    switch ($el.is(':visible')) {
+                      case true:
+                        cy.wrap($el).should('contain', 'No Certificate');
+                        break;
+                    }
+                  });
+                break;
+            }
+          });
       }
     });
   });
--- cypress/support/commands/certificate.ts
+++ cypress/support/commands/certificate.ts
@@ -3,14 +3,14 @@ import 'cypress-file-upload';
 Cypress.Commands.add('certificate', function () {
   cy.fixture('certificate.json').then((data) => {
     data.forEach((step) => {
-      if (step.action === 'click') {
-        return cy.get(step.selector).click().wait(1000);
-      }
-      if (step.action === 'type') {
-        return cy
-          .get(step.selector)
-          .should('be.visible')
-          .type(step.value, { force: true });
+      switch (step.action) {
+        case 'click':
+          cy.get(step.selector).click();
+          break;
+        case 'type':
+          cy.get(step.selector)
+            .should('be.visible')
+            .type(step.value, { force: true });
       }
     });
   });
