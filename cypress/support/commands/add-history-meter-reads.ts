dcff417fc4ecbeb0e589b57b0e62b786b5e459e1
uwumukiza123 <uwumukiza123>
2025-03-24 13:57:09 +0200

    refactor: test cases for adding history meter reads

--- cypress/support/commands/add-history-meter-reads.ts
+++ cypress/support/commands/add-history-meter-reads.ts
@@ -2,68 +2,67 @@ import 'cypress-file-upload';
 Cypress.Commands.add('addHistoryMeterRead', function () {
   cy.fixture('add-history-meter-read.json').then((data) => {
     data.forEach((step) => {
-      if (step.action === 'click') {
-        return cy.get(step.selector).click().wait(1000);
-      }
-      if (step.action === 'selected') {
-        return cy
-          .get(step.selector)
-          .click({ force: true })
-          .wait(1000)
-          .get(step.option)
-          .should('have.length.greaterThan', 0)
-          .first()
-          .should('be.visible')
-          .click({ force: true })
-          .wait(1000);
-      }
-      if (step.action === 'select-timezone') {
-        return cy
-          .get(step.selector)
-          .click({ force: true })
-          .get(step.option)
-          .should('have.length.greaterThan', 0)
-          .eq(0)
-          .click('center', { force: true });
-      }
-      if (step.action === 'select') {
-        return cy
-          .get(step.selector)
-          .click({ force: true })
-          .get(step.option)
-          .should('have.length.greaterThan', 0)
-          .eq(0)
-          .click('center', { force: true });
-      }
+      switch (step.action) {
+        case 'click':
+          cy.get(step.selector).click();
+          break;
 
-      if (step.action === 'type') {
-        return cy
-          .get(step.selector)
-          .wait(1000)
-          .click({ force: true })
-          .type(step.value, { force: true });
-      }
-      if (step.action === 'start-date') {
-        return cy
-          .get(step.selector)
-          .eq(0)
-          .click('center', { force: true })
-          .get(step.option)
-          .eq(0)
-          .click()
-          .get('.mat-stroked-button')
-          .click();
-      }
-      if (step.action === 'end-date') {
-        return cy
-          .get(step.selector)
-          .eq(1)
-          .click('center', { force: true })
-          .get(step.option)
-          .eq(5)
-          .click()
-          .get('.mat-stroked-button')
-          .click();
+        case 'selected':
+          cy.get(step.selector)
+            .click({ force: true })
+            .get(step.option)
+            .should('have.length.greaterThan', 0)
+            .first()
+            .should('be.visible')
+            .click({ force: true })
+            .wait(1000);
+          break;
+
+        case 'select-timezone':
+          cy.get(step.selector)
+            .click({ force: true })
+            .get(step.option)
+            .should('have.length.greaterThan', 0)
+            .eq(0)
+            .click('center', { force: true });
+          break;
+
+        case 'select':
+          cy.get(step.selector)
+            .click({ force: true })
+            .get(step.option)
+            .should('have.length.greaterThan', 0)
+            .eq(0)
+            .click('center', { force: true });
+          break;
+
+        case 'type':
+          cy.get(step.selector)
+            .click({ force: true })
+            .type(step.value, { force: true });
+          break;
+
+        case 'start-date':
+          cy.get(step.selector)
+            .eq(0)
+            .click('center', { force: true })
+            .get(step.option)
+            .eq(0)
+            .click()
+            .get('.mat-stroked-button')
+            .click();
+          break;
+
+        case 'end-date':
+          cy.get(step.selector)
+            .eq(1)
+            .click('center', { force: true })
+            .get(step.option)
+            .eq(5)
+            .click()
+            .get('.mat-stroked-button')
+            .click();
+          break;
       }
     });
   });
