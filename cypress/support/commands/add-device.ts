6e2f5b721d27501b5cd463a1ad5189a6905855ea
uwumukiza123 <uwumukiza123>
2025-03-24 12:13:32 +0200

    refactor: automated test for add devices

--- cypress/support/commands/add-device.ts
+++ cypress/support/commands/add-device.ts
@@ -2,31 +2,34 @@ import 'cypress-file-upload';
 Cypress.Commands.add('addDevice', function () {
   cy.fixture('add-device.json').then((data) => {
     data.forEach((step) => {
-      if (step.action === 'click') {
-        return cy.get(step.selector).click().wait(1000);
-      }
-      if (step.action === 'type') {
-        return cy.get(step.selector).should('be.visible').type(step.value);
-      }
+      switch (step.action) {
+        case 'click':
+          cy.get(step.selector).click();
+          break;
 
-      if (step.action === 'write') {
-        return cy
-          .get(step.selector)
-          .should('be.visible')
-          .clear()
-          .type(step.value + '{enter}');
-      }
-      if (step.action === 'select') {
-        return cy
-          .get(step.selector)
-          .click()
-          .get(step.option)
-          .should('have.length.greaterThan', 0)
-          .eq(0)
-          .click();
-      }
-      if (step.action === 'submit') {
-        return cy.get(step.selector).click('center', { force: true });
+        case 'type':
+          cy.get(step.selector).should('be.visible').type(step.value);
+          break;
+
+        case 'write':
+          cy.get(step.selector)
+            .should('be.visible')
+            .clear()
+            .type(step.value + '{enter}');
+          break;
+
+        case 'select':
+          cy.get(step.selector)
+            .click()
+            .get(step.option)
+            .should('have.length.greaterThan', 0)
+            .eq(0)
+            .click();
+          break;
+
+        case 'submit':
+          cy.get(step.selector).click('center', { force: true });
+          break;
       }
     });
   });
