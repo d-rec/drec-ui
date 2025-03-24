bf6c57cae974102903e214134166a742ce9e4909
uwumukiza123 <uwumukiza123>
2025-03-24 14:44:05 +0200

    refactor: test cases for buyer user permission setup

--- cypress/support/commands/buyer-user-permission-setup.ts
+++ cypress/support/commands/buyer-user-permission-setup.ts
@@ -3,35 +3,31 @@ import 'cypress-file-upload';
 Cypress.Commands.add('buyerUserPermissionsSetup', function () {
   cy.fixture('buyer-user-permissions-setup.json').then((data) => {
     data.forEach((step) => {
-      if (step.action === 'click') {
-        return cy.get(step.selector).wait(10000).should('be.visible').click();
-      }
-      if (step.action === 'select') {
-        return cy
-          .get(step.selector)
-          .click()
-          .get(step.option)
-          .should('have.length.greaterThan', 0)
-          .eq(3)
-          .click()
-          .wait(1000);
-      }
-      if (step.action === 'check') {
-        return cy.get(step.selector).eq(step.index).click();
-      }
-      if (step.action === 'check-multiple') {
-        return cy.contains('table tr', step.contains).within(() => {
-          cy.get(step.selector).each(($el) => {
-            cy.wrap($el).click();
+      switch (step.action) {
+        case 'click':
+          cy.get(step.selector).should('be.visible').click();
+          break;
+        case 'select':
+          cy.get(step.selector)
+            .click()
+            .get(step.option)
+            .should('have.length.greaterThan', 0)
+            .eq(3)
+            .click();
+          break;
+        case 'check':
+          cy.get(step.selector).eq(step.index).click();
+          break;
+        case 'check-multiple':
+          cy.contains('table tr', step.contains).within(() => {
+            cy.get(step.selector).each(($el) => {
+              cy.wrap($el).click();
+            });
           });
-        });
-      }
-      if (step.action === 'submit') {
-        return cy
-          .get(step.selector)
-          .scrollIntoView()
-          .should('be.visible')
-          .click();
+          break;
+        case 'submit':
+          cy.get(step.selector).scrollIntoView().should('be.visible').click();
+          break;
       }
     });
   });
