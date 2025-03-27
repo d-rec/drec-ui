544e1acc77306f67849f632ecdfb167280660947
Laetitia Uwumukiza <113632531+uwumukiza123@users.noreply.github.com>
2025-03-27 10:22:07 +0200

    Feat: split cypress commands according to the feature (#279)
    * feat: split cypress commands according to the feature
    
    * refactor: automated test for adding delta meter reads
    
    * refactor: automated tests for account settings and add aggregate meter reads
    
    * refactor: automated test for add devices
    
    * refactor: test cases for adding history meter reads
    
    * refactor: test cases for adding reservations
    
    * refactor: test cases for admin login
    
    * refactor: test cases for buyer user login
    
    * refactor: test cases for buyer user permission setup
    
    * refactor: test cases for buyer user sign up
    
    * refactor: automated test cases
    
    * refactor: automated test cases
    
    * refactor: automated test cases
    
    * refactor: test cases for certificates
    
    * refactor: automated test cases
    
    * refactor: automated test cases
    
    ---------
    
    Co-authored-by: uwumukiza123 <uwumukiza123>

---
+++ cypress/support/commands/add-device.ts
@@ -0,0 +1,33 @@
+import 'cypress-file-upload';
+Cypress.Commands.add('addDevice', function () {
+  cy.fixture('add-device.json').then((data) => {
+    data.forEach((step) => {
+      switch (step.action) {
+        case 'click':
+          return cy.get(step.selector).click().wait(1000);
+
+        case 'type':
+          return cy.get(step.selector).should('be.visible').type(step.value);
+
+        case 'write':
+          return cy
+            .get(step.selector)
+            .should('be.visible')
+            .clear()
+            .type(step.value + '{enter}');
+
+        case 'select':
+          return cy
+            .get(step.selector)
+            .click()
+            .get(step.option)
+            .should('have.length.greaterThan', 0)
+            .eq(0)
+            .click();
+
+        case 'submit':
+          return cy.get(step.selector).click('center', { force: true });
+      }
+    });
+  });
+});
