f1e63f35538191a1724f3a880e1b9a82c20f0188
uwumukiza123 <uwumukiza123>
2025-04-09 14:29:32 +0200

    refactor: automated test cases

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
   },
   env: {
-    REACT_APP_BACKEND_URL: 'http://localhost:3040',
+    REACT_APP_BACKEND_URL: 'https://stage-api.drecs.org/api/',
     ADMIN_EMAIL: 'stageadmin@drecs.org',
     ADMIN_PASSWORD: 'pass@123',
     UI_BASE_URL: 'http://localhost:4200',
