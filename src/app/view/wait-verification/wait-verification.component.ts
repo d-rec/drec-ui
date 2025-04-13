6322815eeca673df923701e5065db94b23ff5cf0
kingcedru <byishimocedrick.com>
2025-04-13 19:52:12 +0200

    refactor: remove unused routers

--- src/app/view/wait-verification/wait-verification.component.ts
+++ src/app/view/wait-verification/wait-verification.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wait-verification',
   templateUrl: './wait-verification.component.html',
   styleUrls: ['./wait-verification.component.scss'],
 })
-export class WaitVerificationComponent {
-  constructor(private router: Router) {}
-
-  goBack() {
-    this.router.navigate(['/documents-upload']);
-  }
-
-  goToDashboard() {
-    this.router.navigate(['/dashboard']);
-  }
-}
+export class WaitVerificationComponent {}
