c8aec829546dc93a5aa7551580946e0f8d2cf73a
uwumukiza123 <uwumukiza123>
2025-04-14 19:00:50 +0200

    feat: test email verification

--- cypress/support/commands/developer-user-signup.ts
+++ cypress/support/commands/developer-user-signup.ts
@@ -5,6 +5,7 @@ Cypress.Commands.add('developerUserSignup', function () {
   cy.fixture('developer-user-signup.json').then((data) => {
     cy.visit(`${UI_BASE_URL}/login`).wait(1000);
     cy.get('[test-id="register"]').click();
+
     data.forEach((step) => {
       switch (step.action) {
         case 'type':
@@ -20,8 +21,27 @@ Cypress.Commands.add('developerUserSignup', function () {
             .click()
             .then(() => {
               cy.get('mat-option').contains(step.value).click();
-            });
+            })
+            .wait(2000);
       }
+    });
+
+    cy.wait(3000);
+    cy.request('http://localhost:1080/email').then((res) => {
+      const email = res.body.find(
+        (e) => e.to[0].address === 'developer@energy.org',
+      );
+      expect(email).to.exist;
+
+      const linkRegex = /https?:\/\/[^\s"]+/;
+      const emailBody = email.text || email.html;
+      expect(emailBody).to.exist;
+      cy.log(emailBody);
+
+      const verificationLink = emailBody.match(linkRegex)?.[0];
+      expect(verificationLink, 'Verification link should exist in email').to
+        .exist;
+      cy.visit(verificationLink);
     });
   });
 });
