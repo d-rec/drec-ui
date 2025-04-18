87335a09e46f66b6731003e0f6a47229dd5e08e6
uwumukiza123 <uwumukiza123>
2025-04-18 20:09:42 +0200

    fix: developer signup test

--- cypress/support/commands/developer-user-signup.ts
+++ cypress/support/commands/developer-user-signup.ts
@@ -28,25 +28,50 @@ Cypress.Commands.add('developerUserSignup', function () {
 
     cy.wait(3000);
     cy.request('http://localhost:1080/email').then((res) => {
-      const email = res.body.find(
-        (e) => e.to[0].address === 'developer@energy.org',
-      );
-      cy.log('============', JSON.stringify(email), '=================');
-      expect(email).to.exist;
+      // const email = res.body.find(
+      //   (e) => e.to[0].address === 'developer@energy.org',
+      // );
+      // cy.log('============', JSON.stringify(email), '=================');
+      // expect(email).to.exist;
 
-      const linkRegex = /https?:\/\/[^\s"]+/;
-      const emailBody = email.text || email.html;
-      expect(emailBody).to.exist;
-      cy.log(
-        '111111111111111111',
-        JSON.stringify(emailBody),
-        '22222222222222222222222',
-      );
+      // const linkRegex = /https?:\/\/[^\s"]+/;
+      // const emailBody = email.text || email.html;
+      // expect(emailBody).to.exist;
+      // cy.log(
+      //   '111111111111111111',
+      //   JSON.stringify(emailBody),
+      //   '22222222222222222222222',
+      // );
 
-      const verificationLink = emailBody.match(linkRegex)?.[0];
-      expect(verificationLink, 'Verification link should exist in email').to
-        .exist;
-      cy.visit(verificationLink);
+      // const verificationLink = emailBody.match(linkRegex)?.[0];
+      // expect(verificationLink, 'Verification link should exist in email').to
+      //   .exist;
+      // cy.visit(verificationLink);
+
+      cy.waitUntil(
+        () =>
+          cy.request('http://localhost:1080/email').then((res) => {
+            const email = res.body.find(
+              (e) => e.to[0].address === 'developer@energy.org',
+            );
+            return Boolean(email);
+          }),
+        {
+          timeout: 10000,
+          interval: 1000,
+        },
+      ).then(() => {
+        cy.request('http://localhost:1080/email').then((res) => {
+          const email = res.body.find(
+            (e) => e.to[0].address === 'developer@energy.org',
+          );
+          const linkRegex = /https?:\/\/[^\s"]+/;
+          const emailBody = email.text || email.html;
+          const verificationLink = emailBody.match(linkRegex)?.[0];
+          expect(verificationLink).to.exist;
+          cy.visit(verificationLink);
+        });
+      });
     });
   });
 });
