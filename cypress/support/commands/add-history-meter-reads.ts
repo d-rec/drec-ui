ceaac6d1f2bc2990e4e2f15d5b51356ccd42d712
uwumukiza123 <uwumukiza123>
2025-04-24 05:55:16 +0200

    refactor: meter read success messages

--- cypress/support/commands/add-history-meter-reads.ts
+++ cypress/support/commands/add-history-meter-reads.ts
@@ -68,8 +68,7 @@ Cypress.Commands.add('addHistoryMeterRead', function () {
             .wait(1000);
         case 'submit':
           cy.get(step.selector).click('center', { force: true });
-
-          return cy.contains('Read Added!!').should('be.visible');
+          return cy.contains('Successfully!').should('be.visible').wait(20000);
       }
     });
   });
