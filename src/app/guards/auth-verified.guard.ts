a18be3b302e9c0efda4300af78f3165dd898a028
Yannick Musafiri <yannick@musafiri.co>
2025-04-24 09:39:01 +0200

    chore: cleanup

--- src/app/guards/auth-verified.guard.ts
+++ src/app/guards/auth-verified.guard.ts
@@ -2,12 +2,21 @@ import { CanActivateFn } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 
 export const AuthVerifiedGuard: CanActivateFn = (route, state) => {
-  const result = AuthGuard(route, state);
+  // First check if user is logged in
+  const loggedInResult = AuthGuard(route, state);
 
-  if (result !== true) {
-    return result;
+  // If the first check returns false or a UrlTree, return that result
+  if (loggedInResult !== true) {
+    return loggedInResult;
   }
 
+  // const router = inject(Router);
+
   // Additional access checks can go here
+
+  // if (user.emailVerifiedAt === null) {
+  //   router.navigate(['/confirm-email']);
+  // }
+
   return true;
 };
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,10 +2,15 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthbaseService } from '../auth/authbase.service';
 import { Router } from '@angular/router';
-import { checkLoggedIn } from '../utils/authentication-check';
 
 export const AuthGuard: CanActivateFn = () => {
   const authService = inject(AuthbaseService);
   const router = inject(Router);
-  return checkLoggedIn(authService, router);
+  
+  if (!authService.isLoggedIn()) {
+    router.navigate(['/login']);
+    return false;
+  }
+
+  return true;
 };
