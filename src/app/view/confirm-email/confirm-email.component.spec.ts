d29cf420bab9519626b274234d39587646d5b478
kingcedru <byishimocedrick.com>
2025-04-28 17:27:36 +0200

    fix: confirm email tests

--- src/app/view/confirm-email/confirm-email.component.spec.ts
+++ src/app/view/confirm-email/confirm-email.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EmailConfirmedComponent } from './confirm-email.component';
+import { ConfirmEmailComponent } from './confirm-email.component';
 
-describe('EmailConfirmedComponent', () => {
-  let component: EmailConfirmedComponent;
-  let fixture: ComponentFixture<EmailConfirmedComponent>;
+describe('ConfirmEmailComponent', () => {
+  let component: ConfirmEmailComponent;
+  let fixture: ComponentFixture<ConfirmEmailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EmailConfirmedComponent],
+      declarations: [ConfirmEmailComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(EmailConfirmedComponent);
+    fixture = TestBed.createComponent(ConfirmEmailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
