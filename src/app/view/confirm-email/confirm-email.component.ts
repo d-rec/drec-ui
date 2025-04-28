c6dd77cec2af939a4b10ebc6ba65058e6c55082a
Divine Charlotte <divinemaina@gmail.com>
2025-04-28 18:37:15 +0200

    feat: implement the email verification for newly registered users (#291)
    * feat: implement the email verification for newly registered users
    
    * chore: remove unused method
    
    * fix: refactor codes by using meaningfull variable names
    
    * chore: add email confirmation before logging in
    
    * chore:fix confirmation email logic
    
    * feat: add email to the query params
    
    * fix: change the markdown version
    
    * improve the error and text on confirmation email
    
    * fix:prettier errors
    
    * feat: protecting routes with a guard
    
    * refactor: restore removed validations
    
    * feat: allow token to be processed
    
    * feat: log in the user after email confirmation
    
    * chore: clean up
    
    * feat: move confirmation into the login
    
    * feat: created email confirmed page
    
    * feat: add confirmed paged
    
    * chore: remove null checking
    
    * chore: remove the log
    
    * chore: refactor: rename emal confirmed to  confirm email
    
    * fix: confirm email tests
    
    ---------
    
    Co-authored-by: kingcedru <byishimocedrick.com>
    Co-authored-by: Cedrick  Byishimo <byishimocedrick@gmail.com>

---
+++ src/app/view/confirm-email/confirm-email.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/auth/services/user.service';
+
+@Component({
+  selector: 'app-confirm-email',
+  templateUrl: './confirm-email.component.html',
+  styleUrls: ['./confirm-email.component.scss'],
+})
+export class ConfirmEmailComponent implements OnInit {
+  accessToken: any;
+  message: string;
+  success: boolean = true;
+
+  constructor(
+    private userService: UserService,
+    private toastrService: ToastrService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      if (params['token'] != undefined) {
+        this.accessToken = params['token'];
+        this.emailConfirmed(this.accessToken);
+      }
+    });
+  }
+
+  ngOnInit() {}
+
+  emailConfirmed(accessToken: any) {
+    this.userService.confirmEmail(accessToken).subscribe({
+      next: (data: any) => {
+        this.message = data.message;
+
+        this.toastrService.success(' Successful !!', this.message);
+      },
+      error: (err: any) => {
+        this.success = err.error.success;
+        this.message = err.error.message;
+        this.toastrService.error(' Failed !!', this.message);
+      },
+    });
+  }
+}
