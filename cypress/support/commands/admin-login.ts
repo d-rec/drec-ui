d8c908ad02c97ae1d42a9e700d464c951947a696
uwumukiza123 <uwumukiza123>
2025-04-30 14:58:09 +0200

    feat: add automated tests for terms and condition

--- cypress/support/commands/admin-login.ts
+++ cypress/support/commands/admin-login.ts
@@ -4,23 +4,54 @@ const ADMIN_PASSWORD = Cypress.env('ADMIN_PASSWORD');
 const UI_BASE_URL = Cypress.env('UI_BASE_URL');
 
 Cypress.Commands.add('adminLogin', function () {
-  cy.fixture('admin-login.json').then((data) => {
-    cy.visit(`${UI_BASE_URL}/login`).wait(1000);
+  return cy.fixture('admin-login.json').then((data) => {
+    cy.visit(`${UI_BASE_URL}/login`);
 
     data.forEach((step) => {
       switch (step.action) {
         case 'type':
-          return cy
-            .get(step.selector)
-            .type(step.index === 0 ? ADMIN_EMAIL : ADMIN_PASSWORD);
+          cy.get(step.selector).type(
+            step.index === 0 ? ADMIN_EMAIL : ADMIN_PASSWORD,
+          );
+          break;
         case 'click':
           cy.get(step.selector).click();
-          switch (step.selector) {
-            case '[test-id="login-submit"]':
-              cy.contains('Login Success').should('be.visible');
-              cy.contains(`Login user ${ADMIN_EMAIL}`).should('be.visible');
+
+          if (step.selector === '[test-id="login-submit"]') {
+            cy.contains('Login Success').should('be.visible');
+            cy.contains(`Login user ${ADMIN_EMAIL}`).should('be.visible');
           }
+          break;
       }
     });
+
+    cy.get('[test-id="terms-and-conditions"]')
+      .should('exist')
+      .then(($checkbox) => {
+        if ($checkbox.length > 0) {
+          cy.wrap($checkbox).click({ force: true });
+        } else {
+          cy.log('Terms and conditions checkbox not found.');
+        }
+      });
+    cy.get('[test-id="accept-terms-and-conditions"]')
+      .should('exist')
+      .then(($button) => {
+        if ($button.length > 0) {
+          cy.wrap($button).click({ force: true });
+        } else {
+          cy.log('Accept terms and conditions button not found.');
+        }
+      });
+    // cy.get('body').then(($body) => {
+    //   if ($body.find('[test-id="terms-and-conditions"]').length > 0) {
+    //     cy.get('[test-id="terms-and-conditions"]').click({ force: true });
+    //     cy.get('[test-id="accept-terms-and-conditions"]').click({
+    //       force: true,
+    //     });
+    //   } else {
+    //     cy.log('Terms and conditions page not shown (already accepted)');
+    //   }
+    // });
   });
 });
