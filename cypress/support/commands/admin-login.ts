420eab2815aba2f8b7dd499c0eca627f89627790
uwumukiza123 <uwumukiza123>
2025-05-02 09:41:25 +0200

    refactor: automated test cases for terms of condition

--- cypress/support/commands/admin-login.ts
+++ cypress/support/commands/admin-login.ts
@@ -24,24 +24,24 @@ Cypress.Commands.add('adminLogin', function () {
       }
     });
 
-    cy.get('[test-id="terms-and-conditions"]')
-      .should('exist')
-      .then(($checkbox) => {
-        if ($checkbox.length > 0) {
-          cy.wrap($checkbox).click({ force: true });
-        } else {
-          cy.log('Terms and conditions checkbox not found.');
-        }
-      });
-    cy.get('[test-id="accept-terms-and-conditions"]')
-      .should('exist')
-      .then(($button) => {
-        if ($button.length > 0) {
-          cy.wrap($button).click({ force: true });
-        } else {
-          cy.log('Accept terms and conditions button not found.');
-        }
-      });
+    // cy.get('[test-id="terms-and-conditions"]')
+    //   .should('exist')
+    //   .then(($checkbox) => {
+    //     if ($checkbox.length > 0) {
+    //       cy.wrap($checkbox).click({ force: true });
+    //     } else {
+    //       cy.log('Terms and conditions checkbox not found.');
+    //     }
+    //   });
+    // cy.get('[test-id="accept-terms-and-conditions"]')
+    //   .should('exist')
+    //   .then(($button) => {
+    //     if ($button.length > 0) {
+    //       cy.wrap($button).click({ force: true });
+    //     } else {
+    //       cy.log('Accept terms and conditions button not found.');
+    //     }
+    //   });
 
     // cy.get('body').then(($body) => {
     //   if ($body.find('[test-id="terms-and-conditions"]').length > 0) {
