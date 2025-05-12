c7957009b7d49c9304be4ab2b756212ee2cf8421
uwumukiza123 <uwumukiza123>
2025-05-12 09:56:02 +0200

    refactor: buyer user signup

--- cypress/support/commands/developer-user-login.ts
+++ cypress/support/commands/developer-user-login.ts
@@ -1,6 +1,5 @@
 import 'cypress-file-upload';
 const UI_BASE_URL = Cypress.env('UI_BASE_URL');
-const testEmailAddress = `dev1@${Cypress.env('MAILISK_NAMESPACE')}.mailisk.net`;
 
 Cypress.Commands.add('developerUserLogin', function () {
   cy.fixture('developer-user-login.json').then((data) => {
@@ -8,12 +7,6 @@ Cypress.Commands.add('developerUserLogin', function () {
     data.forEach((step) => {
       switch (step.action) {
         case 'type':
-          if (step.selector === "[test-id='login-username']") {
-            return cy
-              .get(step.selector)
-              .type(testEmailAddress)
-              .should('have.value', testEmailAddress);
-          }
           return cy.get(step.selector).type(step.value);
         case 'click':
           return cy.get(step.selector).click().wait(1000);
