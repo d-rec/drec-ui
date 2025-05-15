a9e17410af0996d95be77d8a36f636fb280699f3
kingcedru <byishimocedrick.com>
2025-05-15 23:32:09 +0200

    feat: buyer signup

--- cypress/support/commands/buyer-user-login.ts
+++ cypress/support/commands/buyer-user-login.ts
@@ -8,28 +8,51 @@ Cypress.Commands.add('buyerUserLogin', function () {
       switch (step.action) {
         case 'type':
           return cy.get(step.selector).type(step.value);
+
         case 'click':
-          return cy.get(step.selector).click().wait(1000);
+          return cy.get(step.selector).click({ multiple: true }).wait(3000);
+
+        case 'browse-documents':
+          cy.get(step.selector).each(($input) => {
+            cy.wrap($input).attachFile('files/meter_reads_2025-05-14.pdf', {
+              force: true,
+            });
+          });
+          break;
+
+        case 'verify-phone':
+          const MOCK_OTP_CODE = '123456';
+
+          cy.intercept('POST', `${UI_BASE_URL}/api/otp/send`, {
+            statusCode: 201,
+            body: { message: 'OTP sent (mock)', testCode: MOCK_OTP_CODE },
+          }).as('sendOtp');
+
+          cy.intercept('POST', `${UI_BASE_URL}/api/otp/verify`, (req) => {
+            const { code } = req.body;
+
+            if (code === MOCK_OTP_CODE) {
+              req.reply({
+                statusCode: 200,
+                body: { message: 'Phone number verified successfully.' },
+              });
+            } else {
+              req.reply({
+                statusCode: 400,
+                body: { message: 'Invalid OTP' },
+              });
+            }
+          }).as('verifyOtp');
+
+          cy.get('[test-id="resend-otp"]').click();
+
+          for (let i = 0; i < MOCK_OTP_CODE.length; i++) {
+            cy.get('[test-id="otp-inputs"]').eq(i).type(MOCK_OTP_CODE[i]);
+          }
+
+          cy.get('.otp-container').submit();
+          break;
       }
     });
-
-    cy.get('[test-id="terms-and-conditions"]')
-      .should('exist')
-      .then(($checkbox) => {
-        if ($checkbox.length > 0) {
-          cy.wrap($checkbox).click({ force: true });
-        } else {
-          cy.log('Terms and conditions checkbox not found.');
-        }
-      });
-    cy.get('[test-id="accept-terms-and-conditions"]')
-      .should('exist')
-      .then(($button) => {
-        if ($button.length > 0) {
-          cy.wrap($button).click({ force: true });
-        } else {
-          cy.log('Accept terms and conditions button not found.');
-        }
-      });
   });
 });
