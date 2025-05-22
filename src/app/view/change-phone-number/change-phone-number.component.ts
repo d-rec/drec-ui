269dd5bc25506c5d93066aa4abf4ec39660894fb
kingcedru <byishimocedrick.com>
2025-05-22 15:38:00 +0200

    feat: add change phone number page

---
+++ src/app/view/change-phone-number/change-phone-number.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-phone-number',
+  templateUrl: './change-phone-number.component.html',
+  styleUrls: ['./change-phone-number.component.scss'],
+})
+export class ChangePhoneNumberComponent {}
