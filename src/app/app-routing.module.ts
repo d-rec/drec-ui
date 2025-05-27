bc67c41e91f3318502d1a2a9c1b9df488b7b23ca
Cedrick  Byishimo <byishimocedrick@gmail.com>
2025-05-27 12:12:31 +0200

    feat: add change phone number page (#365)
    * feat: add change phone number page
    
    * feat: send phone number
    
    * feat: add a request url
    
    * refactor: change the url redirection
    
    * refactor: remove unused files
    
    * feat: remove spaces on a phone number
    
    ---------
    
    Co-authored-by: kingcedru <byishimocedrick.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,6 +28,7 @@ import { EmailVerificationGuard } from './guards/verification/email-verified.gua
 import { TermsVerificationGuard } from './guards/verification/terms-verified.guard';
 import { PhoneVerificationGuard } from './guards/verification/phone-verification.guard';
 import { OrganizationDocumentsGuard } from './guards/verification/organization-documents-verified.guard';
+import { ChangePhoneNumberComponent } from './view/change-phone-number/change-phone-number.component';
 
 ('./view/UserAcceptInvitationComponent');
 const routes: Routes = [
@@ -93,6 +94,11 @@ const routes: Routes = [
         path: 'organization/upload/verification-documents',
         component: DocumentsUploadComponent,
         canActivate: [OrganizationDocumentsGuard],
+      },
+      {
+        path: 'change-phone-number',
+        component: ChangePhoneNumberComponent,
+        canActivate: [PhoneVerificationGuard],
       },
     ],
   },
