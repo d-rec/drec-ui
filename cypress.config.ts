e77bc6dce4e43c14d06421736e5e2c35ae7bbd69
uwumukiza123 <uwumukiza123>
2025-06-02 10:39:37 +0200

    refactor: cypress shared user

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,43 +5,6 @@ export default defineConfig({
   e2e: {
     experimentalRunAllSpecs: true,
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
-    setupNodeEvents(on, config) {
-      on('before:run', () => {
-        console.log('All tests done. Cleaning up DB...');
-        try {
-          console.log('Executing: npm run test:e2e (for database cleanup)');
-          execSync('npm run test:e2e', { stdio: 'inherit' });
-          console.log('Database reset after all tests using npm run test:e2e.');
-        } catch (error) {
-          console.error('Failed to reset database after run:', error.message);
-          throw error;
-        }
-      });
-
-      on('task', {
-        resetDbAndSeedUsers() {
-          console.log('Executing resetDbAndSeedUsers task...');
-          try {
-            execSync(
-              'npm run drop && npm run migrate && npm run seed:dummy-data',
-              { stdio: 'inherit' },
-            );
-            console.log('Database reset and seeded via task.');
-            return null;
-          } catch (error) {
-            console.error(
-              'Failed to reset and seed database via task:',
-              error.message,
-            );
-            throw new Error(
-              'Database reset and seed task failed: ' + error.message,
-            );
-          }
-        },
-      });
-
-      return config;
-    },
   },
   env: {
     REACT_APP_BACKEND_URL: 'http://localhost:3040',
