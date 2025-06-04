4197cb1d4fe44ec74969fa2a70ca888a38d777ef
charlotte <divinemaina@gmail.com>
2025-06-04 18:49:35 +0200

    chore:pre-populate the settings page if there are settings that are already existing.

--- src/app/evident-view/settings/settings.component.ts
+++ src/app/evident-view/settings/settings.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { EvidentService } from '../../auth/services/evident.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
-export class SettingsComponent {}
+export class SettingsComponent implements OnInit {
+  @ViewChild('apiKeyInput') apiKeyInput!: ElementRef;
+  @ViewChild('tradingAccountInput') tradingAccountInput!: ElementRef;
+  @ViewChild('beneficiaryAccountInput') beneficiaryAccountInput!: ElementRef;
+
+  constructor(private evidentService: EvidentService) {}
+
+  ngOnInit(): void {
+    this.evidentService.getSettings().subscribe({
+      next: (data) => {
+        if (data) {
+          this.apiKeyInput.nativeElement.value = data.apiKey || '';
+          this.tradingAccountInput.nativeElement.value =
+            data.defaultTradingAccount || '';
+          this.beneficiaryAccountInput.nativeElement.value =
+            data.defaultBeneficiaryAccount || '';
+        }
+      },
+      error: (err) => {
+        console.error('Failed to load settings', err);
+      },
+    });
+  }
+
+  onSubmit(): void {
+    const settings = {
+      apiKey: this.apiKeyInput.nativeElement.value,
+      defaultTradingAccount: this.tradingAccountInput.nativeElement.value,
+      defaultBeneficiaryAccount:
+        this.beneficiaryAccountInput.nativeElement.value,
+    };
+
+    this.evidentService.saveSettings(settings).subscribe({
+      next: () => {
+        this.apiKeyInput.nativeElement.value = '';
+        this.tradingAccountInput.nativeElement.value = '';
+        this.beneficiaryAccountInput.nativeElement.value = '';
+      },
+      error: (err) => {
+        console.error('Error submitting settings:', err);
+      },
+    });
+  }
+}
