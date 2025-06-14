8dbf44729935b12a49685b015a49a40fbb2a87c1
uwumukiza123 <uwumukiza123>
2025-06-14 09:24:03 +0200

    refactor: document uploads on signup

--- cypress/e2e/12-device-bulk-upload.cy.ts
+++ cypress/e2e/12-device-bulk-upload.cy.ts
@@ -3,7 +3,8 @@ describe('Bulk Upload Test', () => {
     cy.wait(1000);
   });
   it('should upload a file', () => {
-    cy.developerUserLogin().wait(30000);
+    // cy.developerUserLogin().wait(30000);
+    cy.adminLogin().wait(20000);
     cy.deviceBulkUpload('device-bulk-upload-template');
   });
 });
