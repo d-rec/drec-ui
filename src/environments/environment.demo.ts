292ab576b349a30373a19606e960e0f7db1828eb
kunda4 <kundaaggy@gmail.com>
2025-06-24 17:19:41 +0200

    ft: add demo environment

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  demo: true,
+  API_URL: 'https://demo-api.drecs.org/api/',
+  Explorer_URL: 'https://volta-explorer.energyweb.org',
+  SENTRY_DNS:
+    'https://5447c8011f4b40f4dcb8742dfbaa1c0e@o4508380579430400.ingest.de.sentry.io/4508380582576208',
+  SENTRY_ENV: 'demo',
+};
