4cdc19ac425d837b1490b550428e3d655c58a8f7
uwumukiza123 <uwumukiza123>
2025-06-27 11:24:42 +0200

    refactor: cypress test cases order

--- cypress/e2e/06.device-registration-on-evident.cy.ts
+++ cypress/e2e/06.device-registration-on-evident.cy.ts
@@ -1,8 +1,8 @@
-describe('Add device Test', () => {
+describe('Add device registration on evident Test', () => {
   beforeEach(() => {
     cy.wait(10000);
   });
-  it('should add new device', () => {
+  it('should test device registration on evident', () => {
     cy.developerUserLogin().wait(6000);
     cy.deviceRegistrationOnEvident();
   });
