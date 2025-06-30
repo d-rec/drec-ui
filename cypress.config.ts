da8190cc58439aedc10a7bab1e5809412c1053ee
uwumukiza123 <uwumukiza123>
2025-06-30 11:34:31 +0200

    refactor: credentials for evident configuration

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+// cypress.config.ts
+import { defineConfig } from 'cypress';
+export default defineConfig({
+  e2e: {
+    experimentalRunAllSpecs: true,
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+  },
+  env: {
+    REACT_APP_BACKEND_URL: 'http://localhost:3040',
+    ADMIN_EMAIL: 'drec@energy.org',
+    ADMIN_PASSWORD: '1234Rc',
+    UI_BASE_URL: 'http://localhost:4200',
+    EVIDENT_API_KEY:
+      '01JY1V3ZQBBCF0Y7E095ZSKRG3.iPIXopj9fiipHXoN97u3Icp9fv9AqybIAHmoKG8rIZfpCahm8',
+    EVIDENT_EMAIL: 'irecregistrantuser9dec8@mail.com',
+  },
+});
