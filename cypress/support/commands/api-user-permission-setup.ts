c96aabb1a3ac1c5ddbb591139eeb0b197669ca16
uwumukiza123 <uwumukiza123>
2025-07-25 17:24:39 +0200

    feat: add automated test cases for api user permission setup

---
+++ cypress/support/commands/api-user-permission-setup.ts
@@ -0,0 +1,23 @@
+Cypress.Commands.add('ApiUserPermissionsSetup', function () {
+  cy.fixture('api-user-permission-setup.json').then((data) => {
+    data.forEach((step) => {
+      switch (step.action) {
+        case 'check':
+          return cy.get(step.selector).eq(step.index).click();
+        case 'check-multiple':
+          return cy.contains('table tr', step.contains).within(() => {
+            cy.get(step.selector).each(($el) => {
+              if (!$el.hasClass('mdc-checkbox--disabled')) {
+                cy.wrap($el).click();
+              } else {
+                cy.log(`Skipping disabled checkbox: ${$el.attr('id')}`);
+              }
+            });
+          });
+        case 'submit':
+          cy.get(step.selector).scrollIntoView().should('be.visible').click();
+          return cy.contains('SuccessFul').should('be.visible');
+      }
+    });
+  });
+});
