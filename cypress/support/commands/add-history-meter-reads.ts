aec2487c5dcdcbb244f2d3b62232324d910b9ea2
uwumukiza123 <uwumukiza123>
2025-07-28 19:16:40 +0200

    feat: add automated test cases for replacing externa ID with serial number

--- cypress/support/commands/add-history-meter-reads.ts
+++ cypress/support/commands/add-history-meter-reads.ts
@@ -1,26 +1,20 @@
-import 'cypress-file-upload';
 Cypress.Commands.add('addHistoryMeterRead', function () {
   cy.fixture('add-history-meter-read.json').then((data) => {
     data.forEach((step) => {
       switch (step.action) {
         case 'click':
-          return cy
-            .get(step.selector)
-            .should('be.visible')
-            .click({ force: true })
-            .wait(5000);
-
+          return cy.get(step.selector).click().wait(1000);
         case 'selected':
           return cy
             .get(step.selector)
             .click({ force: true })
+            .wait(2000)
             .get(step.option)
             .should('have.length.greaterThan', 0)
             .first()
             .should('be.visible')
             .click({ force: true })
-            .wait(500);
-
+            .wait(1000);
         case 'select-timezone':
           return cy
             .get(step.selector)
