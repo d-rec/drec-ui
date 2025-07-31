d04eb45007971d26659477ac98d4cd0ab15cac50
kingcedru <byishimocedrick@gmail.com>
2025-07-31 15:39:23 +0200

    fix: prettier

--- src/app/view/certificate-details/certificate-details.component.ts
+++ src/app/view/certificate-details/certificate-details.component.ts
@@ -267,7 +267,7 @@ export class CertificateDetailsComponent {
       if (selectedorg) {
         this.FilterForm.controls['organizationId'].setValue(selectedorg.id);
       }
-      console.log("selectedorg", this.FilterForm.value)
+      console.log('selectedorg', this.FilterForm.value);
     });
   }
   onEndChangeEvent(event: any) {
